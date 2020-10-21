19adaae448f4b43d463a8cc70d01b9e078bc1994
mlraleader <mouwenlegan@gmail.com>
2020-10-21 15:24:39 +0100

    Suppression du bundle EasyAdmin

---
+++ public/admin-scripts/modal-cdn.js
@@ -0,0 +1,26 @@
+
+    $('#publier-edito').summernote({
+      placeholder: 'Ecrivez ici...',
+      tabsize: 2,
+      height: 200
+    });
+    $('#publier-article').summernote({
+      placeholder: 'Ecrivez ici...',
+      tabsize: 2,
+      height: 200
+    });
+    $('#publier-appel-projet').summernote({
+      placeholder: 'Ecrivez ici...',
+      tabsize: 2,
+      height: 200
+    });
+    $('#publier-evenement').summernote({
+      placeholder: 'Ecrivez ici...',
+      tabsize: 2,
+      height: 200
+    });
+    $('#publier-ressource').summernote({
+      placeholder: 'Ecrivez ici...',
+      tabsize: 2,
+      height: 200
+    });
--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -18,6 +18,8 @@ class RegistrationFormType extends AbstractType
     {
         $builder
             ->add('email')
+            ->add('nom')
+            ->add('telephone')
             ->add('agreeTerms', CheckboxType::class, [
                 'mapped' => false,
                 'constraints' => [
