daef9745ef1c67acc241790599c18d072e3e0829
mlraleader <mouwenlegan@gmail.com>
2020-10-22 12:14:58 +0100

    Insertion des fichiers fonts et création des vue AppelaProjet,QuiSommesNous,CentreDeRecherche

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -16,4 +16,44 @@ class HomeController extends AbstractController
             'controller_name' => 'HomeController',
         ]);
     }
+
+    /**
+     * @Route("/quisommesnous", name="quisommesnous")
+     */
+    public function quisommesnous()
+    {
+        return $this->render('quisommesnous/quisommesnous.html.twig');
+    }
+
+    /**
+     * @Route("/appelaprojet", name="appelaprojet")
+     */
+    public function appelaprojet()
+    {
+        return $this->render('appelaprojet/appelaprojet.html.twig');
+    }
+
+    /**
+     * @Route("/etude", name="etude")
+     */
+    public function etude()
+    {
+        return $this->render('cnetrederechercheaction/etude.html.twig');
+    }
+
+    /**
+     * @Route("/acteurs", name="acteurs")
+     */
+    public function acteurs()
+    {
+        return $this->render('cnetrederechercheaction/acteurs.html.twig');
+    }
+
+    /**
+     * @Route("/fonddocumentaire", name="fonddocumentaire")
+     */
+    public function fonddocumentaire()
+    {
+        return $this->render('cnetrederechercheaction/fonddocumentaire.html.twig');
+    }
 }
